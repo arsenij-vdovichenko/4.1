3309d14b292600234f3962db2f523bdbd2ea9289
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2026-01-04 11:31:25 +0000

    123

--- event.js
+++ event.js
@@ -1,26 +1,26 @@
-const grandfather = document.querySelector(".grandfather")
-const dad = document.querySelector(".dad")
-const son = document.querySelector(".son")
+// const grandfather = document.querySelector(".grandfather")
+// const dad = document.querySelector(".dad")
+// const son = document.querySelector(".son")
 
 
-grandfather.addEventListener("click", grand)
-dad.addEventListener("click", dadfn)
-son.addEventListener("click", sonfn)
+// grandfather.addEventListener("click", grand)
+// dad.addEventListener("click", dadfn)
+// son.addEventListener("click", sonfn)
 
-function grand (event){
-console.log(event.currentTarget);
-console.log(event.target);
-}
+// function grand (event){
+// console.log(event.currentTarget);
+// console.log(event.target);
+// }
 
-function dadfn (event){
-    event.stopImmediatePropagation()
-console.log(event.currentTarget);
-console.log(event.target);
-}
+// function dadfn (event){
+//     event.stopImmediatePropagation()
+// console.log(event.currentTarget);
+// console.log(event.target);
+// }
 
-function sonfn (event){
+// function sonfn (event){
 
     
-console.log(event.currentTarget);
-console.log(event.target);
-}
+// console.log(event.currentTarget);
+// console.log(event.target);
+// }
