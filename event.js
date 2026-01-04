aae76328a7e522774435836885506a8858efe36d
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2026-01-04 11:32:17 +0000

    123

--- event.js
+++ event.js
@@ -24,3 +24,17 @@
 // console.log(event.currentTarget);
 // console.log(event.target);
 // }
+
+
+
+const colors = [
+  { hex: "#FF5733", rgb: "rgb(255, 87, 51)", color: "Red Orange" },
+  { hex: "#33FF57", rgb: "rgb(51, 255, 87)", color: "Lime Green" },
+  { hex: "#3357FF", rgb: "rgb(51, 87, 255)", color: "Royal Blue" },
+  { hex: "#F1C40F", rgb: "rgb(241, 196, 15)", color: "Sunflower Yellow" },
+  { hex: "#9B59B6", rgb: "rgb(155, 89, 182)", color: "Amethyst" },
+  { hex: "#1ABC9C", rgb: "rgb(26, 188, 156)", color: "Turquoise" },
+  { hex: "#E67E22", rgb: "rgb(230, 126, 34)", color: "Carrot Orange" },
+  { hex: "#2C3E50", rgb: "rgb(44, 62, 80)", color: "Midnight Blue" },
+  { hex: "#ECF0F1", rgb: "rgb(236, 240, 241)", color: "Cloud White" }
+];
